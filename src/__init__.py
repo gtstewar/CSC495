dcd0bacf2c85b19e83400aa8a9bfbb3509908065
tonyfloatersu <tonyfloater@gmail.com>
2018-01-21 01:58:16 +0800

    try to remove .idea dir

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# Some notice:
+# we can write some interface or data structure in this file
+# for the further reusage
+
+# we can also define __all__ inside this file
+# without re-import the files again
+# this helps lower the code quantity
