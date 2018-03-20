1fad3fd3972ff6fb30f0311cf0c9afba36dc4285
tonyfloatersu <tonyfloater@gmail.com>
2018-03-21 04:51:05 +0800

    fix error in repr baseclass

--- UtilBasic/BaseObject.py
+++ UtilBasic/BaseObject.py
@@ -2,14 +2,13 @@ class BaseObject(object):
     """The basic object for csc 495 project."""
     def __methods__(self):
         """Show methods for the class"""
-        return '\n'.join([i + ":\n" + i.__doc__ + "\n"
-                          for i in sorted(self.__dict__.keys())
+        return '\n'.join([i for i in sorted(type(self).__dict__)
                           if i[0] is not '_'])
 
     def __repr__(self):
         """Class, Show your identity!"""
         information = "name: " + self.__class__.__name__
-        if self.__doc__.__len__() != 0:
+        if self.__doc__ is not None:
             information += "\n" + self.__doc__
         if self.__methods__().__len__() != 0:
             information += "\n" + "available methods: "
