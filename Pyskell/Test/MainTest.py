d7b75d4ebb0bebf32792f49aae660c79e2a35ea2
Su Hang <tonyfloater@gmail.com>
2018-04-16 14:18:44 +0800

    Concentrate Unit Tests into one place

--- Pyskell/Test/MainTest.py
+++
@@ -1,5 +0,0 @@
-from TestCases import *
-
-
-if __name__ == '__main__':
-    unittest.main()
--- UtilBasic/__init__.py
+++
@@ -1,3 +0,0 @@
-from UtilBasic.BaseObject import *
-from UtilBasic.BaseSettings import *
-from UtilBasic.CommonFunc import *
