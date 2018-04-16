f47018ec5f141323b1412048e42bdaf72f49f4ec
Su Hang <tonyfloater@gmail.com>
2018-04-16 14:10:43 +0800

    Bartok AI tree implemented

--- GlobalConfig/Rules.py
+++ GlobalConfig/Rules.py
@@ -2,8 +2,9 @@ from CardConfig import *
 from random import randint
 
 
-@TS(C / [Card] >> Card >> ([Card], [Card]))
-def bartok_rule_player(card_in_hand, card_face_up):
+@TS(C / [Card] >> [Card] >> ([Card], [Card]))
+def bartok_rule_player(card_in_hand, card_face_ups):
+    card_face_up = card_face_ups[0]
     print "have these cards: "
     print_formatted(hand_show % L[card_in_hand])
     temp = [i for i in card_in_hand
@@ -34,14 +35,9 @@ def bartok_rule_player(card_in_hand, card_face_up):
         return L[[temp[respond]]], L[card_in_hand]
 
 
-# var = bartok_rule_player(L[CD(Club, King), CD(Diamond, Eight)],
-#                          CD(Spade, Seven))
-# print_formatted(show % var[0][0])
-# print_formatted(hand_show % var[1])
-
-
-@TS(C / [Card] >> Card >> ([Card], [Card]))
-def bartok_rule_ai_random(card_in_hand, card_face_up):
+@TS(C / [Card] >> [Card] >> ([Card], [Card]))
+def bartok_rule_ai_random(card_in_hand, card_face_ups):
+    card_face_up = card_face_ups[0]
     temp = [i for i in card_in_hand
             if card_face_up.i0 == i.i0 or
             card_face_up.i1 == i.i1]
@@ -49,6 +45,48 @@ def bartok_rule_ai_random(card_in_hand, card_face_up):
         print "Nothing to toss"
         return L[[]], L[[i for i in card_in_hand]]
     else:
+        respond = randint(0, len(temp) - 1)
+        card_in_hand = [i for i in card_in_hand if i != temp[respond]]
+        return L[[temp[respond]]], L[card_in_hand]
+
+
+@TS(C / [Card] >> [Card] >> ([Card], [Card]))
+def bartok_rule_ai_intelligent(card_in_hand, card_face_ups):
+    card_face_up = card_face_ups[0]
+    temp = [i for i in card_in_hand
+            if card_face_up.i0 == i.i0 or
+            card_face_up.i1 == i.i1]
+    if len(temp) == 0:
+        print "Nothing to toss"
+        return L[[]], L[[i for i in card_in_hand]]
+    else:
+        unit_count_face = 0
+        for i in card_face_ups:
+            if i.i0 == card_face_up.i0:
+                unit_count_face += 1
+        unit_count_hand = 0
+        for i in card_in_hand:
+            if i.i0 == card_face_up.i0:
+                unit_count_hand += 1
+        value_count_face = 0
+        for i in card_face_ups:
+            if i.i1 == card_face_up.i1:
+                value_count_face += 1
+        value_count_hand = 0
+        for i in card_in_hand:
+            if i.i1 == card_face_up.i1:
+                value_count_hand += 1
+        unit_rate = (unit_count_hand + unit_count_face) / 13.0
+        value_rate = (value_count_hand + value_count_face) / 4.0
+        if value_count_hand == 0 and unit_count_hand != 0:
+            temp = [i for i in temp if i.i0 == card_face_up.i0]
+        elif value_count_hand != 0 and unit_count_hand == 0:
+            temp = [i for i in temp if i.i1 == card_face_up.i1]
+        elif value_count_hand != 0 and unit_count_hand != 0:
+            if unit_rate > value_rate:
+                temp = [i for i in temp if i.i1 == card_face_up.i1]
+            else:
+                temp = [i for i in temp if i.i0 == card_face_up.i0]
         respond = randint(0, len(temp) - 1)
         card_in_hand = [i for i in card_in_hand if i != temp[respond]]
         return L[[temp[respond]]], L[card_in_hand]
